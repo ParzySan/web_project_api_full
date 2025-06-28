5015b07fb49bd8d635f030dfcf3cbed3637c3f1c
AlmirCaravantes <andreialmircaravantescaballero@gmail.com>
2025-06-27 19:39:20 -0600

    correcion nuevaurl

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const secret = NODE_ENV === "production" ? JWT_SECRET : "dev-secret";
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://api.aplicacionwebparzy.mooo.com/",
+    origin: "https://aplicacionwebparzy.mooo.com/",
     credentials: true,
   })
 );
